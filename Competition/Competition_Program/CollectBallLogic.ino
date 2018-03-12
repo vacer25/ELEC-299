2840b2d1dc9da7c7cd546c1df5e764008f0d6cec
Ayrtonfoster <ayrtonfoster@gmail.com>
2018-03-12 19:30:42 -0400

    Added Collect Ball Logic file
    Added a preliminaru version of the ball collect logic

---
+++ Competition/Competition_Program/CollectBallLogic.ino
@@ -0,0 +1,28 @@
+
+if(ballLoc = 0) {
+  //pivot right 90 degreese 
+  pivot(true, 90);
+    
+  //drive forward   
+}
+
+else if (ballLoc = 1) {
+  //drive forward          
+}
+
+else if(ballLoc = 2) {
+  //pivot left 90 degreese
+  pivot(false, 90);   
+  
+  //drive forward    
+  drive(true,);
+}
+
+/*void setup() {
+  
+}
+
+void loop() {
+
+}*/ 
+
