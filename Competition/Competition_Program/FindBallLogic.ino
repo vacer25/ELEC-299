c77c6027a139d90b3a2bc973a7c6ae95debef23e
Eric Leask <16efl@queensu.ca>
2018-03-12 21:26:21 -0400

    Updates to FindBallLogic

--- Competition/Competition_Program/FindBallLogic.ino
+++ Competition/Competition_Program/FindBallLogic.ino
@@ -4,56 +4,46 @@ void FindBallLogic{
 
 //Note:U/D servo pin 9
 //Note:L/R servo pin 10
-#define byte ANG1=0;
-#define byte ANG2=1;
-bool ang1Result=False;
-bool ang2Result=False;
+#define ANG1 0;
+#define ANG2 1;
+bool ang1Result=false;
+bool ang2Result=false;
 byte loc=ANG1;
 int val;
 unsigned waitMillis;
 int delayMillis=410;
-void setup() {
+
   pinMode(IR_IN_PIN, INPUT);
   //pinMode(BUZZER_PIN, OUTPUT);
   myIRserial.attach(IR_IN_PIN, -1);
   Serial.begin(115200);
 
-  /*
-     tone(BUZZER_PIN, 200, 50);
-     delay(1000);
-     tone(BUZZER_PIN, 700, 50);
-     delay(1000);
-     tone(BUZZER_PIN, 1500, 50);
-     delay(1000);
-     tone(BUZZER_PIN, 2500, 200);
-  */
-}
-
 while(state=0){
-if (loc=ANG1){
+if (loc==ANG1){
  pivotSearchArm(0,10);
 //Look in range of 0-10
 //Look in range of 99-106
 loc=ANG2;
 }
-if (loc=ANG2)
+if (loc==ANG2)
 pivotSearchArm(99,104);
 loc=ANG1;
 }
 }
-void pivotSearchArm(int start,int end){
-  for(start;start<end;start++){
-    waitMillis=millis()+delayMillis;
-    while(millis()<delayMillis){
-    LRServo.write(start);
-    IRcheck();
+void pivotSearchArm(int start, int end) {
+  for (int currentLoc=start; currentLoc < end; currentLoc++) {
+    waitMillis = millis() + delayMillis;
+    while (millis() < delayMillis) {
+      LRServo.write(currentLoc);
+      IRcheck();
     }
-    }
+  }
 }
-void IRcheck(){
-val = myIRserial.receive(200);
-  if (val>=48&&val<=50) {
-    ballLoc=(char)val;
-    state=1;
+void IRcheck() {
+  
+  val = myIRserial.receive(200);
+  if (val >= 48 && val <= 50) {
+    ballLoc = val-48;
+    state = 1;
   }
-  }
+}
