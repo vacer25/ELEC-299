28e466dc82e4e8ecb7b90a6add956b2d42c19030
vacer25 <vacer25@gmail.com>
2018-03-13 09:40:52 -0400

    Updated Deposit Ball Logic to set state back to 0 at end.

--- Competition/Competition_Program/2_DepositBallLogic.ino
+++ Competition/Competition_Program/2_DepositBallLogic.ino
@@ -61,4 +61,7 @@ void runDepositBallLogic() {
   // At this point the robot has reached the center, so stop it
   stopMotors();
 
+  // Set state to 0 so robot can start the find ball code
+  state = 0;
+
 }
